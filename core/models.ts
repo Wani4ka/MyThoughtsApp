45a00f5d03d56941e3e8ee675b32afd39a7777ad
Wani4ka <contact.wani4ka@gmail.com>
2022-03-30 04:37:39 +0300

    🐛 Fixed database url format

--- core/models.ts
+++ core/models.ts
@@ -3,7 +3,7 @@ import config from '../config'
 export default new PrismaClient({
 	datasources: {
 		db: {
-			url: `mysql://${config.db.user}:${config.db.pass}@${config.db.host}:${config.db.port}/${config.db.database}`
+			url: `mysql://${config.db.user}:${config.db.pass}@${config.db.host}:${config.db.port}/${config.db.name}`
 		}
 	}
 })
